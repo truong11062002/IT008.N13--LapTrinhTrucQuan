23766c7fc312e15bcc9d847563b6493477704ee3
nn_truong1106 <truong11062002@gmail.com>
2022-10-15 18:57:37 +0700

    init game tetris

---
+++ Tetris_game/Tetris_game/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Tetris_game
+{
+    class Tetris : Form
+    {
+        static void Main(string[] args)
+        {
+            Tetris form = new Tetris();
+            Application.Run(form);
+        }
+    }
+
+
+}
