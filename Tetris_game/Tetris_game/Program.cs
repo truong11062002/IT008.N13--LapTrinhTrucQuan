f6cbbe9fe2d701c1ea1ecd7ec3049a1ef2fd069f
nn_truong1106 <truong11062002@gmail.com>
2022-10-20 14:06:19 +0700

    dev: init chess_game

--- Tetris_game/Tetris_game/Program.cs
+++ Tetris_game/Tetris_game/Program.cs
@@ -8,12 +8,38 @@ using System.Drawing;
 
 namespace Tetris_game
 {
-    class Tetris : Form
+    public partial class Tetris : Form
     {
+
+        int[,] map = new int[8, 16];
+        public Tetris()
+        {
+            this.Text = "Tetris game";
+            this.Height = 600;
+            this.Width = 600;
+            this.CenterToScreen();
+        }
+
+        public void init()
+        {
+
+        }
+        public void DrawMap(Graphics g)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for(int j =0; j< 16; j++)
+                {
+                    g.DrawLine();
+                }
+            }
+        }
+
+        
         static void Main(string[] args)
         {
-            Tetris form = new Tetris();
-            Application.Run(form);
+            Tetris f_tetris = new Tetris();
+            Application.Run(f_tetris);
         }
     }
 
