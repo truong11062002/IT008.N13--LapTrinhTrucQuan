c98a976d05d1d96ea7328aafc58eaae59eb6be23
nn_truong1106 <truong11062002@gmail.com>
2022-11-14 14:23:24 +0700

    dev: complete bt09

---
+++ Thực hành/BTTH03/BTTH03/Bai09/Modify.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Bai09
+{
+    class Modify
+    {
+        SqlDataAdapter dataAdapter; // Truy xuat du lieu vao bang
+        SqlCommand sqlCommand;
+        public Modify()
+        {
+
+        }
+        public DataTable getAllSinhVien()
+        {
+            DataTable dataTable = new DataTable();
+            string query = "select * from SINHVIEN";
+            //------------------------------------------
+            using (SqlConnection sqlConnection = Connection.getConnection())
+            {
+                sqlConnection.Open();
+                //------------------------------------------
+                dataAdapter = new SqlDataAdapter(query, sqlConnection);
+                dataAdapter.Fill(dataTable);
+                //------------------------------------------
+                sqlConnection.Close();
+            }
+            return dataTable;
+        }
+
+        public bool insert(SinhVien sinhvien)
+        {
+            SqlConnection sqlConnection = Connection.getConnection();
+            string query = "insert into SINHVIEN values(@MSSV, @HoTen, @ChuyenNganh, @GioiTinh, @SoMon)";
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.Parameters.Add("@MSSV", SqlDbType.Char).Value = sinhvien.Mssv;
+                sqlCommand.Parameters.Add("@HoTen", SqlDbType.NVarChar).Value = sinhvien.Hoten;
+                sqlCommand.Parameters.Add("@ChuyenNganh", SqlDbType.NVarChar).Value = sinhvien.Chuyennganh;
+                sqlCommand.Parameters.Add("@GioiTinh", SqlDbType.NVarChar).Value = sinhvien.Gioitinh;
+                sqlCommand.Parameters.Add("@SoMon", SqlDbType.Int).Value = sinhvien.Somon;
+                sqlCommand.ExecuteNonQuery(); // Thuc thi cau lenh truy van
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return true;
+        }
+
+        public bool delete(string id)
+        {
+            SqlConnection sqlConnection = Connection.getConnection();
+            string query = "delete SINHVIEN WHERE MSSV = @MSSV";
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.Parameters.Add("@MSSV", SqlDbType.Char).Value = id;
+                sqlCommand.ExecuteNonQuery(); // Thuc thi cau lenh truy van
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return true;
+        }
+    }
+    
+}
---
+++ Thực hành/BTTH03/BTTH03/Bai09/SinhVien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai09
+{
+    class SinhVien
+    {
+        private string _mssv;
+        private string _hoten;
+        private string _chuyennganh;
+        private string _gioitinh;
+        private string _somon;
+
+        public SinhVien()
+        {
+
+        }
+        public SinhVien(string mssv, string hoten, string chuyennganh, string gioitinh, string somon)
+        {
+            _mssv = mssv;
+            _hoten = hoten;
+            _chuyennganh = chuyennganh;
+            _gioitinh = gioitinh;
+            _somon = somon;
+        }
+
+
+        public string Mssv { get => _mssv; set => _mssv = value; }
+        public string Hoten { get => _hoten; set => _hoten = value; }
+        public string Chuyennganh { get => _chuyennganh; set => _chuyennganh = value; }
+        public string Gioitinh { get => _gioitinh; set => _gioitinh = value; }
+        public string Somon { get => _somon; set => _somon = value; }
+    }
+}
